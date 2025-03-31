961be9e296bddd03304a20c61c0eff18207ec38a
maracevedo_meli <martin.acevedo@mercadolibre.com>
2025-03-30 23:15:45 -0300

    Add extra solution for two sum

--- two_sum_02/solution.go
+++ two_sum_02/solution.go
@@ -19,14 +19,12 @@ func twoSum(nums []int, target int) []int {
 func twoSum2(nums []int, target int) []int {
 	numMap := make(map[int]int)
 	for i, num := range nums {
+		numMap[num] = i
+	}
+	for i, num := range nums {
 		complement := target - num
-		if _, ok := numMap[complement]; !ok {
-			numMap[num] = i
-		}
-		if j, ok := numMap[complement]; ok {
-			if i != j {
-				return []int{j, i}
-			}
+		if j, ok := numMap[complement]; ok && j != i {
+			return []int{i, j}
 		}
 	}
 	return []int{}
